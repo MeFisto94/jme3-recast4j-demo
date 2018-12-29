411b0095f385a475007c5ba0720d984202ffa624
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-12-30 00:02:49 +0100

    Push Demo Application with a new version of the bindings exposing the debug meshes graphically

--- src/main/java/com/jme3/recast4j/demo/DemoApplication.java
+++ src/main/java/com/jme3/recast4j/demo/DemoApplication.java
@@ -9,15 +9,16 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.post.FilterPostProcessor;
 import com.jme3.post.ssao.SSAOFilter;
-import com.jme3.recast4j.Recast.RecastConfigBuilder;
-import com.jme3.recast4j.Recast.RecastTest;
-import com.jme3.recast4j.Recast.RecastUtils;
+import com.jme3.recast4j.Recast.*;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 import org.recast4j.detour.MeshData;
 import org.recast4j.detour.NavMesh;
+import org.recast4j.detour.NavMeshBuilder;
+import org.recast4j.recast.RecastBuilder;
+import org.recast4j.recast.RecastBuilderConfig;
 
 public class DemoApplication extends SimpleApplication {
 
@@ -36,15 +37,13 @@ public class DemoApplication extends SimpleApplication {
     public void simpleInitApp() {
         character = (Node)assetManager.loadModel("Models/Jaime.j3o");
         character.setLocalTranslation(0f, 5f, 0f);
-        character.setLocalScale(0.5f);
-
 
         Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         mat.setColor("Diffuse", ColorRGBA.Red);
         mat.setColor("Ambient", ColorRGBA.White);
         mat.setBoolean("UseMaterialColors", true);
         //worldMap = (Geometry)assetManager.loadModel("Models/dune.j3o");
-        worldMap = new Geometry("", new Box(64f, 1f, 64f));
+        worldMap = new Geometry("", new Box(8f, 1f, 8f));
         //worldMap.setLocalScale(0.01f);
         worldMap.setMaterial(mat);
         // @TODO: Dune.j3o does not have normals and thus no neat lighting.
@@ -61,34 +60,48 @@ public class DemoApplication extends SimpleApplication {
         rootNode.attachChild(character);
         rootNode.attachChild(worldMap);
 
-        getCamera().setLocation(new Vector3f(0f, 10f, 0f));
+        getCamera().setLocation(new Vector3f(0f, 20f, 0f));
         getCamera().lookAtDirection(new Vector3f(0f, -1f, 0f), Vector3f.UNIT_Z);
 
         fpp = new FilterPostProcessor(assetManager);
         viewPort.addProcessor(fpp);
         fpp.addFilter(new SSAOFilter(1f, 1f, 0.1f, 0.1f));
 
-        System.err.println("Building Nav Mesh, this may freeze your computer for a few seconds, please stand by");
+        System.out.println("Building Nav Mesh, this may freeze your computer for a few seconds, please stand by");
         long time = System.currentTimeMillis(); // Never do real benchmarking with currentTimeMillis!
-        MeshData meshData = RecastTest.buildBlockingRenderThread(worldMap.getMesh());
-        navMesh = new NavMesh(meshData, 0, 0);
+        //MeshData meshData = RecastTest.buildBlockingRenderThread(worldMap.getMesh());
+        RecastBuilderConfig bcfg = new RecastBuilderConfigBuilder(worldMap).build(new RecastConfigBuilder().withVertsPerPoly(3).build());
+        MeshData meshData = NavMeshBuilder.createNavMeshData(new NavMeshDataCreateParamsBuilder(new RecastBuilder().build(new GeometryProviderBuilder(worldMap).build(), bcfg)).build(bcfg));
+        navMesh = new NavMesh(meshData, bcfg.cfg.maxVertsPerPoly, 0);
+
         try {
             RecastTest.saveToFile(navMesh);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
+        showDebugMeshes(meshData);
+        System.out.println("Building succeeded after " + (System.currentTimeMillis() - time) + " ms");
+    }
+
+    private void showDebugMeshes(MeshData meshData) {
         Material matRed = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         matRed.setColor("Color", ColorRGBA.Red);
-        // navMesh.getTile(0).data == meshData (in this particular case)
-        Geometry g = new Geometry("DebugMesh", RecastUtils.getDebugMesh(meshData.detailMeshes, meshData.detailVerts, meshData.detailTris));
-        g.setMaterial(matRed);
-        //g.setLocalScale(0.01f);
-        System.out.println(g.getVertexCount());
-        g.move(0f, 0.1f, 0f);
-        rootNode.attachChild(g);
+        Material matGreen = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        matGreen.setColor("Color", ColorRGBA.Green);
 
-        System.err.println("Building succeeded after " + (System.currentTimeMillis() - time) + " ms");
+        // navMesh.getTile(0).data == meshData (in this particular case)
+        Geometry gDetailed = new Geometry("DebugMeshDetailed", RecastUtils.getDebugMesh(meshData.detailMeshes, meshData.detailVerts, meshData.detailTris));
+        Geometry g = new Geometry("DebugMeshSimple", RecastUtils.getDebugMesh(meshData));
+        g.setMaterial(matRed);
+        gDetailed.setMaterial(matGreen);
+        System.out.println("VertCount Regular Mesh: " + g.getVertexCount());
+        System.out.println("VertCount Detailed Mesh: " + gDetailed.getVertexCount());
+        g.move(0f, 0.5f, 0f);
+        gDetailed.move(0f, 1f, 0f);
+
+        rootNode.attachChild(g);
+        rootNode.attachChild(gDetailed);
     }
 
     @Override
