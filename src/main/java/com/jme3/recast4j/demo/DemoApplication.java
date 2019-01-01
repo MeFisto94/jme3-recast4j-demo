3d616394fdb587b4efec33c7edb9b92e1deae862
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-01-01 14:00:27 +0100

    Demo Application now shows boxes for the start and end of a path and also refactored some code.

--- src/main/java/com/jme3/recast4j/demo/DemoApplication.java
+++ src/main/java/com/jme3/recast4j/demo/DemoApplication.java
@@ -19,11 +19,12 @@ import com.jme3.scene.shape.Box;
 import com.simsilica.lemur.GuiGlobals;
 import com.simsilica.lemur.event.DefaultMouseListener;
 import com.simsilica.lemur.event.MouseEventControl;
-import org.recast4j.detour.MeshData;
-import org.recast4j.detour.NavMesh;
-import org.recast4j.detour.NavMeshBuilder;
+import org.recast4j.detour.*;
 import org.recast4j.recast.RecastBuilder;
 import org.recast4j.recast.RecastBuilderConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DemoApplication extends SimpleApplication {
 
@@ -32,7 +33,11 @@ public class DemoApplication extends SimpleApplication {
     NavMesh navMesh;
     FilterPostProcessor fpp;
     Node character;
+    List<Geometry> boxes;
 
+    public DemoApplication() {
+        boxes = new ArrayList<>(64);
+    }
 
     public static void main(String[] args) {
         DemoApplication app = new DemoApplication();
@@ -42,7 +47,43 @@ public class DemoApplication extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         GuiGlobals.initialize(this);
+        flyCam.setEnabled(false);
 
+        setupWorld();
+
+        System.out.println("Building Nav Mesh, this may freeze your computer for a few seconds, please stand by");
+        long time = System.currentTimeMillis(); // Never do real benchmarking with currentTimeMillis!
+        RecastBuilderConfig bcfg = new RecastBuilderConfigBuilder(worldMap).build(new RecastConfigBuilder().withVertsPerPoly(3).build());
+        MeshData meshData = NavMeshBuilder.createNavMeshData(new NavMeshDataCreateParamsBuilder(new RecastBuilder().build(new GeometryProviderBuilder(worldMap).build(), bcfg)).build(bcfg));
+        navMesh = new NavMesh(meshData, bcfg.cfg.maxVertsPerPoly, 0);
+
+        try {
+            RecastTest.saveToFile(navMesh);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        showDebugMeshes(meshData);
+        System.out.println("Building succeeded after " + (System.currentTimeMillis() - time) + " ms");
+
+        MouseEventControl.addListenersToSpatial(worldMap, new DefaultMouseListener() {
+            @Override
+            protected void click(MouseButtonEvent event, Spatial target, Spatial capture) {
+                super.click(event, target, capture);
+                // First clear existing boxes from the old path finding:
+                boxes.forEach(Geometry::removeFromParent);
+
+                // Clicked on the map, so build a path to:
+                rootNode.attachChild(placeColoredBoxAt(ColorRGBA.Green, character.getWorldTranslation().add(0f, 0.5f, 0f)));
+                rootNode.attachChild(placeColoredBoxAt(ColorRGBA.Yellow, getLocationOnMap().add(0f, 0.5f, 0f)));
+
+                NavMeshQuery
+
+            }
+        });
+    }
+
+    private void setupWorld() {
         character = (Node)assetManager.loadModel("Models/Jaime.j3o");
         character.setLocalTranslation(0f, 5f, 0f);
 
@@ -73,31 +114,6 @@ public class DemoApplication extends SimpleApplication {
         fpp = new FilterPostProcessor(assetManager);
         viewPort.addProcessor(fpp);
         fpp.addFilter(new SSAOFilter(1f, 1f, 0.1f, 0.1f));
-
-        System.out.println("Building Nav Mesh, this may freeze your computer for a few seconds, please stand by");
-        long time = System.currentTimeMillis(); // Never do real benchmarking with currentTimeMillis!
-        //MeshData meshData = RecastTest.buildBlockingRenderThread(worldMap.getMesh());
-        RecastBuilderConfig bcfg = new RecastBuilderConfigBuilder(worldMap).build(new RecastConfigBuilder().withVertsPerPoly(3).build());
-        MeshData meshData = NavMeshBuilder.createNavMeshData(new NavMeshDataCreateParamsBuilder(new RecastBuilder().build(new GeometryProviderBuilder(worldMap).build(), bcfg)).build(bcfg));
-        navMesh = new NavMesh(meshData, bcfg.cfg.maxVertsPerPoly, 0);
-
-        try {
-            RecastTest.saveToFile(navMesh);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        showDebugMeshes(meshData);
-        System.out.println("Building succeeded after " + (System.currentTimeMillis() - time) + " ms");
-
-        MouseEventControl.addListenersToSpatial(worldMap, new DefaultMouseListener() {
-            @Override
-            protected void click(MouseButtonEvent event, Spatial target, Spatial capture) {
-                super.click(event, target, capture);
-                // Clicked on the map, so build a path to:
-                System.out.println(getLocationOnMap());
-            }
-        });
     }
 
     private void showDebugMeshes(MeshData meshData) {
@@ -121,13 +137,10 @@ public class DemoApplication extends SimpleApplication {
     }
 
     @Override
-    public void simpleUpdate(float tpf) {
-        //TODO: add update code
-    }
+    public void simpleUpdate(float tpf) { }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
     }
 
     protected void walk(boolean walking) {
@@ -138,6 +151,12 @@ public class DemoApplication extends SimpleApplication {
         }
     }
 
+    /**
+     * Returns the Location on the Map which is currently under the Cursor. For this we use the Camera to project the
+     * point onto the near and far plane (because we don't have the depth information [map height]). Then we can use
+     * this information to do a raycast, ideally the world is in between those planes and we hit it at the correct place.
+     * @return The Location on the Map
+     */
     public Vector3f getLocationOnMap() {
         Vector3f worldCoordsNear = getCamera().getWorldCoordinates(inputManager.getCursorPosition(), 0);
         Vector3f worldCoordsFar = getCamera().getWorldCoordinates(inputManager.getCursorPosition(), 1);
@@ -152,5 +171,23 @@ public class DemoApplication extends SimpleApplication {
         } else {
             return null;
         }
+    }
+
+    /**
+     * Helper method to place a colored box at a specific location and will the boxes list with it, so that later on we
+     * can remove all existing boxes (from a previous path finding)
+     *
+     * @param color The color the box should have
+     * @param position The position where the box will be placed
+     * @return the box
+     */
+    public Geometry placeColoredBoxAt(ColorRGBA color, Vector3f position) {
+        Geometry result = new Geometry("Box", new Box(0.25f, 0.25f, 0.25f));
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", color);
+        result.setMaterial(mat);
+        result.setLocalTranslation(position);
+        boxes.add(result);
+        return result;
     }
 }
