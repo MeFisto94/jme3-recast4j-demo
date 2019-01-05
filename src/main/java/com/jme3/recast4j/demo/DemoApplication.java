4f19df8297b33e0795ce93b2deb4a4ee7badfbd1
MeFisto94 <MeFisto94@users.noreply.github.com>
2019-01-05 23:18:36 +0100

    Refactoring and showing the path taken properly by using an offset so it's not obstructed by the DebugMeshView.

--- src/main/java/com/jme3/recast4j/demo/DemoApplication.java
+++ src/main/java/com/jme3/recast4j/demo/DemoApplication.java
@@ -73,8 +73,8 @@ public class DemoApplication extends SimpleApplication {
         //Load or build mesh objects used for navigation here.
         //Must be called prior to running recast navMesh build procedure.
         //Must set worldMap variable from method call.
-//        loadNavMeshBox();
-//        loadNavMeshDune();
+        //        loadNavMeshBox();
+        //        loadNavMeshDune();
         loadNavMeshLevel();
         loadDoors();
         
@@ -109,34 +109,38 @@ public class DemoApplication extends SimpleApplication {
                 FindNearestPolyResult startPoly = query.findNearestPoly(character.getWorldTranslation().toArray(null), new float[] {2f, 2f, 2f}, filter);
                 FindNearestPolyResult endPoly = query.findNearestPoly(getLocationOnMap().toArray(null), new float[] {2f, 2f, 2f}, filter);
 
-                FindPathResult fpr = query.findPath(startPoly.getNearestRef(), endPoly.getNearestRef(), startPoly.getNearestPos(), endPoly.getNearestPos(), filter);
-                if (fpr.getStatus().isSuccess()) {
-                    // Get the proper path from the rough polygon listing
-                    List<StraightPathItem> list = query.findStraightPath(startPoly.getNearestPos(), endPoly.getNearestPos(), fpr.getRefs(), Integer.MAX_VALUE, 0);
-                    Vector3f oldPos = character.getWorldTranslation();
-                    List<Vector3f> vector3fList = new ArrayList<>(list.size());
-
-                    if (!list.isEmpty()) {
-                        for (StraightPathItem p: list) {
-                            Vector3f nu = DetourUtils.createVector3f(p.getPos());
-                            rootNode.attachChild(placeColoredLineBetween(ColorRGBA.Orange, oldPos, nu));
-                            if (p.getRef() != 0) { // if ref is 0, it's the end.
-                                rootNode.attachChild(placeColoredBoxAt(ColorRGBA.Blue, nu));
-                            }
-                            vector3fList.add(nu);
-                            oldPos = nu;
-                        }
-
-                        character.getControl(NavMeshChaserControl.class).stopFollowing();
-                        character.getControl(NavMeshChaserControl.class).followPath(vector3fList);
-                    } else {
-                        System.err.println("Unable to find straight paths");
-                    }
-                } else {
-                    System.err.println("I'm sorry, unable to find a path.....");
-                }
+                findPathImmediately(filter, startPoly, endPoly);
             }
         });
+    }
+
+    private void findPathImmediately(QueryFilter filter, FindNearestPolyResult startPoly, FindNearestPolyResult endPoly) {
+        FindPathResult fpr = query.findPath(startPoly.getNearestRef(), endPoly.getNearestRef(), startPoly.getNearestPos(), endPoly.getNearestPos(), filter);
+        if (fpr.getStatus().isSuccess()) {
+            // Get the proper path from the rough polygon listing
+            List<StraightPathItem> list = query.findStraightPath(startPoly.getNearestPos(), endPoly.getNearestPos(), fpr.getRefs(), Integer.MAX_VALUE, 0);
+            Vector3f oldPos = character.getWorldTranslation();
+            List<Vector3f> vector3fList = new ArrayList<>(list.size());
+
+            if (!list.isEmpty()) {
+                for (StraightPathItem p: list) {
+                    Vector3f nu = DetourUtils.createVector3f(p.getPos());
+                    rootNode.attachChild(placeColoredLineBetween(ColorRGBA.Orange, oldPos.add(0f, 0.5f, 0f), nu.add(0f, 0.5f, 0f)));
+                    if (p.getRef() != 0) { // if ref is 0, it's the end.
+                        rootNode.attachChild(placeColoredBoxAt(ColorRGBA.Blue, nu.add(0f, 0.5f, 0f)));
+                    }
+                    vector3fList.add(nu);
+                    oldPos = nu;
+                }
+
+                character.getControl(NavMeshChaserControl.class).stopFollowing();
+                character.getControl(NavMeshChaserControl.class).followPath(vector3fList);
+            } else {
+                System.err.println("Unable to find straight paths");
+            }
+        } else {
+            System.err.println("I'm sorry, unable to find a path.....");
+        }
     }
 
     private void setupWorld() {
